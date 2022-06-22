f0505fc0f535bfbe5cb16fb99ca8fe2705a8db93
Ali Amori Kadhim <ali@frameless.io>
2022-06-22 15:36:43 +0000

    docs: add the correct path to the stencil build

--- packages/iconset-web-component/src/component_templates.js
+++ packages/iconset-web-component/src/component_templates.js
@@ -51,7 +51,7 @@ exports.generateIconsSetPage = (iconsNames) => {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <script type="module" src="../../../dist/icons/icons.esm.js"></script>
+    <script type="module" src="../../dist/esm/icons.js"></script>
     <title>Opengemeenten/iconset</title>
 
     <style>
