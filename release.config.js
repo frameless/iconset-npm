3299a760664047f5043830ed0c52b65201fc37d0
Ali Amori Kadhim <ali@frameless.io>
2022-06-22 15:36:43 +0000

    chore(root): add semantic-release-monorepo-hooks

--- release.config.js
+++ release.config.js
@@ -1,22 +1,31 @@
+const hooks = require('semantic-release-monorepo-hooks')
+const output = hooks()
+
 module.exports = {
-  branches: ['main'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
+  branch: 'master',
+  tagFormat: 'v${version}',
+  prepare: [
+    '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'dist/**'],
-        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    {
+      'path': '@semantic-release/git',
+      'message': 'chore(' + output.package + '): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }
   ],
+  publish: [
+    '@semantic-release/npm'
+  ],
+  verifyConditions: [
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  monorepo: {
+    analyzeCommits: [
+      '@semantic-release/commit-analyzer'
+    ],
+    generateNotes: [
+      '@semantic-release/release-notes-generator'
+    ]
+  }
 };
