6622bdc103b7c89b8e40bed980058936a0a7af76
Ali Amori Kadhim <ali@frameless.io>
2022-06-23 16:36:42 +0200

    docs: add more info to the iconset page

--- packages/iconset-web-component/src/component_templates.js
+++ packages/iconset-web-component/src/component_templates.js
@@ -59,7 +59,7 @@ exports.generateIconsSetPage = (iconsNames) => {
         --opengemeenten-icon-size: 3rem;
         --opengemeenten-icon-color: tomato;
       }
-      h1,
+      h1,h2, h4,
       p {
         font-family: 'Roboto', sans-serif;
       }
@@ -91,25 +91,103 @@ exports.generateIconsSetPage = (iconsNames) => {
 
 <body class="container">
 <h1>Opengemeenten/iconset</h1>
+
+<h2>How to use the iconset library</h2>
+
+<section>
+<h3>HTML, or any Frameworks</h3>
+
+<pre>
+<code style="white-space: pre;">
+<span>// Import the library by putting the script in the HTML &lt;head/&gt;</span>
+<br/>
+ <span>&lt;script src="https://unpkg.com/@opengemeenten/iconset-web-component/dist/esm/icons.js" type="module"&gt; &lt;/script&gt;</span>
+<br/>
+<span>Then you will be able to use the icon in the page</span>
+<br/>
+<span>&lt;opengemeenten-icon-aanmelden&gt;&lt;/opengemeenten-icon-aanmelden&gt;</span>
+</code>
+</pre>
+</section>
+
+<section>
+<h3>React, Gatsbyjs, Nextjs </h3>
+<pre>
+<code style="white-space: pre;">
+   <span>
+    npm install @opengemeenten/iconset-web-component 
+    #or
+    yarn add @opengemeenten/iconset-web-component
+   </span> 
+   <br/>
+   import React from "react";
+   import {OpengemeentenIconActueel} from "@opengemeenten/iconset-react"; 
+
+   export const App = () => {
+     return &lt;OpengemeentenIconActueel /&gt;
+   }
+</code>
+</pre>
+</section>
+
+<section>
+<h3>Style</h3>
+<p>All what you need to change the size, color, just call these variables in your CSS file, it can be in the root or scooped</p>
+
+<pre>
+<code style="white-space: pre;">
+:root {
+  --opengemeenten-icon-size: 3rem;
+  --opengemeenten-icon-color: tomato;
+}
+</code>
+</pre>
+</section>
+
+<section>
+<h3>Accessibility</h3>
+
+<pre>
+<code style="white-space: pre;">
+Web component
+
+<span>&lt;opengemeenten-icon-aanmelden&gt role="img" aria-labelledby="aanmelden icon";&lt;/opengemeenten-icon-aanmelden&gt;</span>
+
+
+Reactjs
+
+<span>&lt;OpengemeentenIconActueel role="img" aria-labelledby="Actueel Icon" /&gt;</span>
+</code>
+</pre>
+</section>
+
+<section>
+
+<h3>The iconset list</h3>
+
     <div class="grid-container">${iconsNames
       .map(
         (svg) => `<details> 
-        <summary><${svg}></${svg}>
-         <p>${svg}</p></summary>
-         HTML: <pre>
-         <code>
-         // Import the library by putting the script in the html head
-         &lt;script src="https://unpkg.com/@opengemeenten/iconset-web-component/dist/esm/icons.js" type="module"&gt; &lt;/script&gt;
-
-         Then you will be able to use the icon in the page 
-         &lt;${svg}&gt;&lt;/${svg}&gt;
+        <summary>
+        <${svg}></${svg}>
+          <p>${svg}</p></summary>
+          <strong>HTML:</strong> 
+          <pre>
+            <code style="white-space: pre;">
+         <span>&lt;${svg}&gt;&lt;/${svg}&gt;</span>
          </code>
          </pre>
-         React: <pre>...</pre>
+        <strong> React, Gatsbyjs, Nextjs </strong>
+         <pre>
+         <code style="white-space: pre;">
+         <span>&lt;${upperFirst(camelCase(svg))} /&gt;</span>
+         </code>
+         </pre>
         </details>`,
       )
       .join('')}
     </div>
+    </section>
 </body>
 </html>
   `;
